2a8fd7d6f42d881c66ec992459229bffa10d3308
Maciej Jurkowski <Maciej.Jurkowski@Emerson.com>
2023-02-14 22:17:38 +0100

    Adding project with console access to NASA Horizons System database

---
+++ HorizonsDataConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HorizonsData;
+using HorizonsDataConsole;
+using RealData;
+
+Console.WriteLine("\n\nWelcome to the JPL Horizon System's console interface\n\n");
+
+Console.WriteLine("Choose the celestial object to pull data for:");
+var selection = ProgramSubroutines.CelestialObjectSelection();
+
+Console.WriteLine("Set time and date to pull data for:");
+var dateTime = ProgramSubroutines.DateTimeSelection();
+
+var h = new HorizonsTelnetInterface();
+var data = await h.ReadDataFromHorizonsAsync(selection, dateTime);
+
+CelestialModel celestialObject;
+celestialObject = RealDataReader.ReadRealCelestialDataFromStringData(data);
+
+Console.Clear();
+Console.WriteLine($"\n{celestialObject.Name}\n");
+Console.WriteLine($"Mass {celestialObject.GetMass(MassUnits.KG)} KG");
+Console.WriteLine("\nPosition");
+Console.WriteLine($"X - {celestialObject.GetX(PositionUnits.AU)} AU");
+Console.WriteLine($"Y - {celestialObject.GetY(PositionUnits.AU)} AU");
+Console.WriteLine($"Z - {celestialObject.GetZ(PositionUnits.AU)} AU");
+Console.WriteLine("\nSpeed");
+Console.WriteLine($"V_X - {celestialObject.GetV_X(SpeedUnits.AU_DAYS)} AU/day");
+Console.WriteLine($"V_Y - {celestialObject.GetV_Y(SpeedUnits.AU_DAYS)} AU/day");
+Console.WriteLine($"V_Z - {celestialObject.GetV_Z(SpeedUnits.AU_DAYS)} AU/day");
---
+++ HorizonsDataConsole/ProgramSubroutines.cs
@@ -0,0 +1,46 @@
+﻿using HorizonsData;
+
+namespace HorizonsDataConsole;
+
+internal static class ProgramSubroutines
+{
+    internal static int CelestialObjectSelection()
+    {
+        (string? RawValue, int Value, bool IsValid) selection = (null, 0, false);
+        do
+        {
+            foreach (var option in HorizonsTelnetInterface.HorizonsDataIds)
+                Console.WriteLine($"\t{option.Value}\t-\t{option.Key}");
+
+            selection.RawValue = Console.ReadLine();
+            if (int.TryParse(selection.RawValue, out selection.Value))
+                if (HorizonsTelnetInterface.HorizonsDataIds.ContainsValue(selection.Value))
+                {
+                    selection.IsValid = true;
+                    break;
+                }
+
+            Console.WriteLine("Invalid selection. Please choose from the available options:");
+        } while (!selection.IsValid);
+
+        return selection.Value;
+    }
+
+    internal static DateTime DateTimeSelection()
+    {
+        (string? RawValue, DateTime Value, bool IsValid) dateTimeSelection = (null, new DateTime(), false);
+        do
+        {
+            dateTimeSelection.RawValue = Console.ReadLine();
+            if (DateTime.TryParse(dateTimeSelection.RawValue, out dateTimeSelection.Value))
+            {
+                dateTimeSelection.IsValid = true;
+                break;
+            }
+
+            Console.WriteLine("Invalid date and time format. Please try again.");
+        } while (!dateTimeSelection.IsValid);
+
+        return dateTimeSelection.Value;
+    }
+}
